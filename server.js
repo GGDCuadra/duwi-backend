9eea5ca2fbb800934c1e5d40d95fe908692ed9ec
unknown <ddnchi2@gmail.com>
2023-10-12 22:21:32 -0500

    actualizado

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
-const express = require('express');
+const express = require("express");
+
 const app = express();
+
 const port = process.env.PORT || 3001;
 
+
+app.use(express.json());
 const mongoose = require('mongoose');
 mongoose.connect('mongodb+srv://DBUSER:PF123@cluster0.x6eafwv.mongodb.net/DB_PF', { useNewUrlParser: true, useUnifiedTopology: true });
 
@@ -11,10 +15,10 @@ mongoose.connection.on('connected', () => {
 
 const moviesRoutes = require('./src/Routes/MoviesRoutes');
 const seriesRoutes = require('./src/Routes/SeriesRoutes');
-
 app.use('/', moviesRoutes);
 app.use('/', seriesRoutes);
 
 app.listen(port, () => {
   console.log(`El servidor está escuchando en el puerto ${port}`);
 });
+module.exports = app;
