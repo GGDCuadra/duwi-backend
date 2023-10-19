49a0e6ac2adec43b312292f22532d7933267501a
unknown <ddnchi2@gmail.com>
2023-10-19 10:52:59 -0500

    rutas de favoritos

--- src/Routes/MoviesRoutes.js
+++ src/Routes/MoviesRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const moviesController = require('../Controllers/MoviesControllers');
+const favController = require('../Controllers/FavControllersMovies');
 
 const router = express.Router();
 
@@ -15,5 +16,12 @@ router.get('/movies/byObjectId/:id', moviesController.getMovieByObjectId);
 
 router.post('/movies', moviesController.postMovie);
 router.put('/movies/byObjectId/:id', moviesController.putMovie); 
+
+
+//RUTAS FAVORITOS DE MOVIES
+router.post('/favorites/movies', favController.addMovieToFavorite);
+router.get('/favorites/movies/:userId', favController.getFavoriteMoviesByUser);
+router.delete('/favorites/movies/:userId/:movieId', favController.deleteMovieFromFavorite);
+
 
 module.exports = router;
