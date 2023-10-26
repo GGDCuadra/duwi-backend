551b8701f3c47371059daf6137d89ac986ec6b35
Alejandra <aleja98riano@gmail.com>
2023-10-26 10:17:46 -0500

    Ruta todas donaciones

--- src/Routes/DonationRoutes.js
+++ src/Routes/DonationRoutes.js
@@ -5,5 +5,6 @@ const router = express.Router();
 
 router.post('/donate', DonationController.postDonation);
 router.get('/viewdonations/:userId', DonationController.viewDonations);
+router.get('/alldonations', DonationController.getAllDonations);
 
 module.exports = router;
